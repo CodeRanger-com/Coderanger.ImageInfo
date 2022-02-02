d581b8a5b058836f8654bedea0da5487927e6c90
Dan Petitt <danp@coderanger.com>
2022-02-02 20:35:51 +0000

    chore(Workflows): Build and Release

---
+++ .github/UpdateVersion.ps1
@@ -0,0 +1,15 @@
+﻿$project = "<Project>`n" +
+"  <PropertyGroup>`n" +
+"    <Product>Coderanger.ImageInfo</Product>`n" +
+"    <Authors>CodeRanger</Authors>`n" +
+"    <Company>CodeRanger</Company>`n" +
+"    <Copyright>Copyright (c) CodeRanger. All rights reserved.</Copyright>`n" +
+"    <Version>$($env:GITVERSION_SEMVER)</Version>`n" +
+"    <FileVersion>$($env:GITVERSION_SEMVER)</FileVersion>`n" +
+"    <InformationalVersion>$($env:GITVERSION_SEMVER)</InformationalVersion>`n" +
+"  </PropertyGroup>`n" +
+"</Project>"
+
+$project | Out-File -FilePath './Directory.Build.props';
+Write-Host "Outputting new Build.props:"
+Write-Host $project
